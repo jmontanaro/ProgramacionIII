c8c30b8f289aa4422e8a90a54eecb4dc1c73f8c1
jmontanaro <juan.montanaro@gmail.com>
2015-09-11 17:49:40 -0300

    console.log

--- webapp-node/App.js
+++ webapp-node/App.js
@@ -2,7 +2,8 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
+  console.log(req);
 })
 
 var server = app.listen(80, function () {
