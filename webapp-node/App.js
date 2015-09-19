572b8dea54eca7a5f4563d52f6f7fd9f8108dbad
Juan Francisco Montanaro <jmontanaro@users.noreply.github.com>
2015-09-18 21:24:56 -0300

    Update App.js

--- webapp-node/App.js
+++ webapp-node/App.js
@@ -21,5 +21,6 @@ var server = app.listen(80, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port);
+})
 
