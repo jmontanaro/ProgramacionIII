b63532e34c72d2213b16035c77f1a18189b14552
unknown <rossi.renzo@maimonides.edu>
2015-10-02 20:38:30 -0300

    Tp andando

--- rossi.renzo/Agenda/src/com/Renzo/Contactos.java
+++ rossi.renzo/Agenda/src/com/Renzo/Contactos.java
@@ -2,25 +2,26 @@ package com.Renzo;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 
 
 @Path("/contactos")
 public class Contactos {
 
+	
 	@POST
-	public String procesarDatos (@FormParam("Nombre") String nombre, @FormParam("Apellido") String apellido, @FormParam("Direccion") String direccion, @FormParam("Tel")  int telefono){
-		System.out.println("Nombre: " + nombre);
-		System.out.println("Apellido: " + apellido);
-		System.out.println("Direccion: " + direccion);
-		System.out.println("Telefono: " + telefono);
-	return "Hola Mundo " + nombre + apellido + direccion + telefono;		
+	public String procesarDatos (@FormParam("Nombre") String Nombre, 
+								 @FormParam("Apellido") String Apellido, 
+								 @FormParam("Direccion") String Direccion, 
+								 @FormParam("Tel")  String Telefono){
+		System.out.println("Nombre: " + Nombre);
+		System.out.println("Apellido: " + Apellido);
+		System.out.println("Direccion: " + Direccion);
+		System.out.println("Telefono: " + Telefono);
+	
+	return "Nombre: " + Nombre + " Apellido: " + Apellido + " Direccion: " + Direccion + " Telefono: " + Telefono;		
 	} 
 	
-	 @GET
-		 public String datosProcesados(){
-		 return "Datos Procesados";
-	 }
+
 	
 }
