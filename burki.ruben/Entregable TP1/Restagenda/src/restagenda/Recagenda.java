e9baa34a1a5ef82b5de74d286be1695e9ef482a4
rubenburki <burki.ruben@maimonides.edu>
2015-10-03 18:43:48 -0300

    Se agrega return con String

--- burki.ruben/Entregable TP1/Restagenda/src/restagenda/Recagenda.java
+++ burki.ruben/Entregable TP1/Restagenda/src/restagenda/Recagenda.java
@@ -8,13 +8,13 @@ import javax.ws.rs.FormParam;
 public class Recagenda {
 
 	@POST
-	public void imprimirdatosformulario (@FormParam("Apellido") String surname, @FormParam("Nombre") String name, 
+	public String imprimirdatosformulario (@FormParam("Apellido") String surname, @FormParam("Nombre") String name, 
 			                             @FormParam("Direccion") String address, 
 			                             @FormParam("Telefono") String telephone ) {
 		System.out.println( "Apellido: " + surname );
 		System.out.println( "Nombre: " + name );
 		System.out.println( "Direccion: " + address );
 		System.out.println( "Telefono: " + telephone );
-		System.out.println( "Metodo ejecutado" );
+		return "Metodo ejecutado";
 	} 
 }
